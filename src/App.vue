6da6a6ae94d84043e9f13a8955269593cf033c78
Greg <gregory.marshall999@gmail.com>
2024-10-21 14:29:01 +0200

    computed

--- src/App.vue
+++ src/App.vue
@@ -6,37 +6,57 @@
 </form>
 
 <ul>
-	<li v-for="t in taches" :key="t.id">
-		{{ t.text }}
+	<li v-for="t in displayTasks" :key="t.id">
+		<input type="checkbox" v-model="t.done"/>
+		<span :class="{ done: t.done }">{{ t.text }}</span>
 		<button @click="removeTask(t)">X</button>
 	</li>
 </ul>
 
+<button @click="hideTasks = !hideTasks">
+	{{ hideTasks ?  'Afficher taches' : 'Cacher taches' }}
+</button>
+
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 let id = 0;
 const taches = ref([
-	{ id: id++, text: 'Faire le lit' }, 
-	{ id: id++, text: 'Faire le petit dejeuner' },
-	{ id: id++, text: 'Aller au travail' }
+	{ id: id++, text: 'Faire le lit', done: true }, 
+	{ id: id++, text: 'Faire le petit dejeuner', done: true },
+	{ id: id++, text: 'Aller au travail', done: false }
 ]);
 
 const nTask = ref('');
 const addTask = () => {
-	taches.value.push({ id: id++, text: nTask.value });
+	taches.value.push({ id: id++, text: nTask.value, done: false });
 	nTask.value = '';
 };
 
 const removeTask = task => {
-	console.log(task);
 	taches.value = taches.value.filter(t => t !== task);
 };
+
+const hideTasks = ref(false);
+const displayTasks = computed(() => {
+	return hideTasks.value ? 
+		taches.value.filter(t => !t.done) : 
+		taches.value;
+});
 </script>
 
 <style scoped>
+
+span {
+	margin-left: 0.5em;
+	margin-right: 0.5em;
+}
+
+.done {
+	text-decoration: line-through;
+}
 
 header {
   line-height: 1.5;
