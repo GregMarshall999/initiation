9fc1470c3254523b8a5057db4e08cd0e97b8304f
Greg <gregory.marshall999@gmail.com>
2024-10-22 11:24:16 +0200

    interception

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Contact from '@/views/Contact.vue'
 import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
 import MessageContact from '@/views/MessageContact.vue'
 import NotFound from '@/views/NotFound.vue'
+import Profil from '@/views/Profil.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -24,6 +26,22 @@ const router = createRouter({
       props: true
     }, 
     {
+      path: '/login', 
+      name: 'Login', 
+      component: Login, 
+      meta: {
+        titrePage: 'Se connecter'
+      }
+    },
+    {
+      path: '/profil', 
+      name: 'Profil',
+      component: Profil, 
+      meta: {
+        titrePage: 'Mon Profil'
+      }
+    },
+    {
       path: '/:pathMatch(.*)*', 
       name: 'NotFound', 
       component: NotFound
@@ -31,7 +49,21 @@ const router = createRouter({
   ]
 })
 
+router.beforeEach((to, _, next) => {
+  const titlePages = ['Login', 'Profil'];
 
+  if(titlePages.includes(to.name)) {
+    document.title = to.meta.titrePage;
+  }
+  else {
+    document.title = 'Vue Initiation';
+  }
 
+  if(to.name === 'Profil' && (!localStorage.connecte || localStorage.connecte === 'false')) {
+    next({ name: 'Home' });
+  } else {
+    next();
+  }
+});
 
 export default router
